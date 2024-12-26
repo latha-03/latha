260f288eba9eb663cbe6759d565b7636ac8a0ca7
latha-03 <pushpalathatammina2003@gmail.com>
2024-12-26 16:04:10 +0530

    first commit

---
+++ src/main/java/com/sathya/sprinbootmvc/SpringBootmvcApplication.java
@@ -0,0 +1,13 @@
+package com.sathya.sprinbootmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootmvcApplication.class, args);
+	}
+
+}
